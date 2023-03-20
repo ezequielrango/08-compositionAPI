47b1b3c6ab714e54b3228d93f2db7c46b5768881
Ezequiel Rango <ezequielrango29@gmail.com>
2023-03-20 01:10:13 -0300

    lifecycle hooks

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,73 @@
 <template>
 
     <div class="about">
-        <h1>This is an about page</h1>
+        <h1>{{title}}</h1>
+        <button @click="changeTitle">Cambiar titulo</button>
     </div>
 </template>
 <script>
+import { 
+    onBeforeMount,
+    onMounted,
+    onBeforeUpdate,
+    onUpdated,
+    onBeforeUnmount,
+    onErrorCaptured,
+    onUnmounted,
+    onRenderTracked,
+    onRenderTriggered,
+    onActivated,
+    onDeactivated,
+    ref
+
+} from 'vue';
+
 export default {
+
+    //no hay que meclar los ciclos de vida de composition API con los de optionsAPI
     setup() {
 
+        const title = ref('About')
+        
+        onBeforeMount(() => {
+            console.log('onBeforeMount');
+        })
+        onMounted(() => {
+            console.log('onMounted');
+        })
+        onBeforeUpdate(() => {
+            console.log('obBeforeUpdate');
+        })
+        onUpdated(() => {
+            console.log('OnUpdated');
+        })
+        onBeforeUnmount(() => {
+          console.log('onBeforeUnmount');  
+        })
+        onUnmounted(() => {
+            console.log('OnUnmounted');
+        })
+        onErrorCaptured(() => {
+            console.log('onErrorCaptured');
+        })
+        onRenderTracked(() => {
+            console.log('onRenderTracked');
+        })
+        onRenderTriggered(() => {
+            console.log('OnRenderTriggered');
+        })
+        onActivated(() => {
+            console.log('onActivated');
+        })
+        onDeactivated(() => {
+            console.log('onDeactivated');
+        })
+        return {
+            title,
+            changeTitle : () => {
+                title.value = 'Titulo cambiado'
+            }
+        }
     },
 }
 </script>
