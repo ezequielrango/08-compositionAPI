763cc1423df9f967b289e309c4d6f163a9c1dbb8
Ezequiel Rango <ezequielrango29@gmail.com>
2023-03-19 21:50:59 -0300

    create counterComponent

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -1,10 +1,36 @@
 <template>
     <h1>Counter view</h1>
+    <h2>{{counter}}</h2>
+
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+
 </template>
 
 <script>
-    export default{
+import { ref } from 'vue';
 
+    export default{
+        name: 'Counter',
+        props: {},
+        emits: [],
+        setup() {
+            const counter = ref(5)
+
+
+            const increase = () => {
+                counter.value++
+            }
+
+            const decrease = () => {
+                counter.value--
+            }
+            return {
+                counter,
+                increase,
+                decrease
+            }
+        }
     }
 </script>
 <style>
