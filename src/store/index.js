865674d17666387ca31faae2506cf55f4db646f6
Ezequiel Rango <ERango@proveedor.teco.com.ar>
2023-03-23 17:30:09 -0300

    set state store

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,33 @@ import { createStore } from "vuex";
 
 export default createStore({
     state: {
-
+        todos: [
+            {
+                id: '1',
+                text: 'Recolectar las gemas del infinito',
+                completed: false
+            },
+            {
+                id: '2',
+                text: 'Piedra del alma',
+                completed: false
+            },
+            {
+                id: '3',
+                text: 'Piedra del tiempo',
+                completed: false
+            },
+            {
+                id: '4',
+                text: 'Piedra de la realidad',
+                completed: false
+            },
+            {
+                id: '5',
+                text: 'Piedra del poder',
+                completed: false
+            },
+        ]
     },
     mutations: {
 
@@ -11,6 +37,6 @@ export default createStore({
 
     },
     modules: {
-        
+
     }
 })
