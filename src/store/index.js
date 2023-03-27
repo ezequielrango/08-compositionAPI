e19ba33e71eaa9dda818a6ddbb68a4508f777272
Ezequiel Rango <ERango@proveedor.teco.com.ar>
2023-03-26 22:48:59 -0300

    commit mutations

--- src/store/index.js
+++ src/store/index.js
@@ -26,20 +26,38 @@ export default createStore({
             {
                 id: '5',
                 text: 'Piedra del poder',
-                completed: false
+                completed: true
             },
         ]
     },
     mutations: {
+        toggleTodo(state, id){
+            const todoIdx = state.todos.findIndex(todo => todo.id === id)
 
+            state.todos[todoIdx].completed = !state.todos[todoIdx].completed
+        }   
     },
     actions: {
 
     },
     getters:{
-        pendingTodos(state, getters){
+        pendingTodos(state, getters, rootState){
             return state.todos.filter(todo => !todo.completed)
+        },
+        allTodos(state, getters, rootState){
+            return state.todos
+        },
+        completedTodos(state, getters, rootState){
+            return state.todos.filter(todo => todo.completed)
+        },
+        getTodosByTab: ( state , getters, rootState) => ( tab ) => {
+            switch ( tab ) {
+                case 'all': return getters.allTodos
+                case 'pending': return getters.pendingTodos    
+                case 'completed': return getters.completedTodos
+            }
         }
+
     },
     modules: {
 
--- src/views/TodoVuex.vue
+++ src/views/TodoVuex.vue
@@ -1,13 +1,38 @@
 <template>
     <h1>Lista de tareas de Thanos</h1>
     <h4>Tareas pendientes: {{pending.length}} </h4>
-    <ul>
-        <li v-for="todo in pending" :key="todo.id + Math.random()"> Pendiente: {{ todo.text }}</li>
-    </ul>
+    <div>
+        <button 
+            :class="{'active' : currentTab === 'all'}"
+            @click="currentTab = 'all'"
+            >Todos
+        </button>
+        <button 
+            :class="{'active' : currentTab === 'pending'}"
+            @click="currentTab = 'pending'"
+            >Pendientes
+        </button>
+        <button 
+            :class="{'active' : currentTab === 'completed'}"
+            @click="currentTab = 'completed'"
+            >Completados
+        </button>
+    </div>
+
+    <div>
+        <ul>
+            <li v-for="todo in getTodosByTab" :key="todo.id"
+                :class="{'completed' : todo.completed }"
+                @dblclick="toggleTodo(todo.id)"
+            >
+                {{todo.text }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import {computed} from 'vue'
+import {computed,ref} from 'vue'
 import {useStore} from 'vuex'
 
 export default {
@@ -15,15 +40,35 @@ export default {
 
 
         const store = useStore()
-        
-        return{
-            //el getter es un objeto del cual quiero obtener los pending todo. Es reactivo 
-            pending: computed(() => store.getters['pendingTodos'])
+        const currentTab = ref('all')
 
+        const toggleTodo = ( id ) => {
+            store.commit('toggleTodo', id)
+        }
+
+        return{
+            currentTab,
+            //el getter es un objeto del cual quiero obtener los pending todo. Es reactivo 
+            pending: computed(() => store.getters['pendingTodos']),
+            all : computed(() => store.getters['allTodos']),
+            completed: computed(() => store.getters['completedTodos']),
+            
+
+            getTodosByTab: computed( () => store.getters['getTodosByTab'](currentTab.value)),
+            toggleTodo: (id) => {
+                store.commit('toggleTodo', id)
+            }
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
+.active{
+    background-color: #2c3e50;
+    color: white;
+}
+.completed{
+    text-decoration: line-through;
+}
 </style>
